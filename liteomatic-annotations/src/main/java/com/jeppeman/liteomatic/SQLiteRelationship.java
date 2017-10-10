1ae4e54d3441ea06487d5ab40738a30d4faf616a
jeppeman <jesperaamann@gmail.com>
2017-10-10 09:23:05 +0200

    Update @SQLiteRelationship to handle OneToOne or OneToMany

--- liteomatic-annotations/src/main/java/com/jeppeman/liteomatic/SQLiteRelationship.java
+++ liteomatic-annotations/src/main/java/com/jeppeman/liteomatic/SQLiteRelationship.java
@@ -8,5 +8,11 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.FIELD)
 public @interface SQLiteRelationship {
-    Class<?> table();
+
+    RelationshipType type() default RelationshipType.OneToMany;
+
+    enum RelationshipType {
+        OneToOne,
+        OneToMany
+    }
 }
